a798fa1db62286fc7aabe3839069c73c2d5ede5c
Devy <devywhite771@gmail.com>
2024-10-31 00:22:28 +0100

    new reciple add

--- src/components/imageMap.js
+++ src/components/imageMap.js
@@ -19,6 +19,7 @@ import imgBolognese_tofu from "../images/bolognese_tofu.png";
 import imgShake_owocowy_z_bananem_i_jagodami from "../images/shake_owocowy_z_bananem_i_jagodami.png";
 import imgChia_pudding_dyniowy from "../images/chia_pudding_dyniowy.jpeg";
 import imgMakaron_z_dynia_szpinakiem_i_serem_feta from "../images/makaron_z_dynia_szpinakiem_i_serem_feta.jpeg";
+import imgZapiekanka_makaronowa_z_wazywami_i_serem from "../images/zapiekanka_makaronowa_z_wazywami_i_serem.jpeg";
 
 // Mapowanie identyfikatorów obrazków na zaimportowane obrazy
 const imageMap = {
@@ -44,6 +45,8 @@ const imageMap = {
    chia_pudding_dyniowy: imgChia_pudding_dyniowy,
    makaron_z_dynia_szpinakiem_i_serem_feta:
       imgMakaron_z_dynia_szpinakiem_i_serem_feta,
+   zapiekanka_makaronowa_z_wazywami_i_serem:
+      imgZapiekanka_makaronowa_z_wazywami_i_serem,
 };
 
 export default imageMap;
